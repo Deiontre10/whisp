41b61c336e2d60d7eea37feb05709a7f1526976e
Taz Lowry <travontazlowry@gmail.com>
2023-05-18 18:38:32 -0400

    setup input.tsx and added an email input on authform

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -67,7 +67,7 @@ const AuthForm = () => {
       sm:px-10"
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input />
+          <Input id="email" label="Email" register={register} errors={errors} />
         </form>
       </div>
     </div>
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,5 +1,73 @@
-const Input = () => {
-  return <div>Hey</div>;
+"use client";
+
+import clsx from "clsx";
+import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}) => {
+  return (
+    <div>
+      <label
+        htmlFor={id}
+        className="
+      block 
+      text-sm 
+      font-medium 
+      text-gray-700
+      "
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          className={clsx(
+            `
+            form-input
+            block
+            w-full
+            rounded-md
+            border-0
+            py-1.5
+            text-gray-900
+            shadow-sm
+            ring-1
+            ring-insert
+            ring-gray-300
+            placeholder:text-gray-400
+            focus:ring-2
+            focus:ring-inset
+            focus:ring-indigo-500
+            sm:text-sm
+            sm:leading-5`,
+            errors[id] && "focus:ring-red-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Input;
